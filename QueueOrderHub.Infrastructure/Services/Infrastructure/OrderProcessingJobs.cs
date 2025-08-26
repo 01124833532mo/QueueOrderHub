496503e371783493d58e13f80530c7974fcad087
mohamed_hamdi_mahmoud <mohammedhamdi726@gmail.com>
2025-08-26 16:38:22 +0300

    Add Consumming At Order Status With HangFire

--- QueueOrderHub.Infrastructure/Services/Infrastructure/OrderProcessingJobs.cs
+++ QueueOrderHub.Infrastructure/Services/Infrastructure/OrderProcessingJobs.cs
@@ -1,6 +1,7 @@
 ﻿using Hangfire;
 using Microsoft.Extensions.Logging;
 using QueueOrderHub.Core.Domain.Contracts.Infrastructure;
+using QueueOrderHub.Core.Domain.Models.Messages;
 using QueueOrderHub.Core.Domain.Models.Orders;
 
 namespace QueueOrderHub.Infrastructure.Services.Infrastructure
@@ -16,10 +17,39 @@ namespace QueueOrderHub.Infrastructure.Services.Infrastructure
             _orderRepository = orderRepository;
         }
         [AutomaticRetry(Attempts = 3)]
-        public async Task ProcessOrderAsync(Order order)
+        public async Task ProcessOrderAsync(OrderMessage orderMessage)
         {
-            order.Status = OrderStatus.Processing;
-            await _orderRepository.UpdateOrderStatusAsync(order);
+
+            var order = _orderRepository.GetOrderStatusAsync(orderMessage.OrderId);
+            if (order is null)
+            {
+                var newOrder = new Order
+                {
+                    Id = orderMessage.OrderId,
+                    CustomerName = orderMessage.CustomerName,
+                    Product = orderMessage.Product,
+                    Quantity = orderMessage.Quantity,
+                    TotalAmount = orderMessage.TotalAmount,
+                    Status = OrderStatus.Pending,
+                    CreatedAt = orderMessage.CreatedAt
+                };
+                await _orderRepository.StoreOrderStatusAsync(newOrder);
+            }
+
+            await _orderRepository.UpdateOrderStatusAsync(new Order
+            {
+                Id = orderMessage.OrderId,
+                CustomerName = orderMessage.CustomerName,
+                Product = orderMessage.Product,
+                Quantity = orderMessage.Quantity,
+                TotalAmount = orderMessage.TotalAmount,
+                Status = OrderStatus.Processing,
+                CreatedAt = orderMessage.CreatedAt
+            });
+
+
+
+
 
         }
 
--- QueueOrderHub.Infrastructure/Services/Infrastructure/RabbitMQService.cs
+++ QueueOrderHub.Infrastructure/Services/Infrastructure/RabbitMQService.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Hangfire;
+using Microsoft.Extensions.Configuration;
 using QueueOrderHub.Core.Domain.Contracts.Infrastructure;
 using QueueOrderHub.Core.Domain.Models.Messages;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using System.Text;
 using System.Text.Json;
 
@@ -31,6 +33,8 @@ namespace QueueOrderHub.Infrastructure.Services.Infrastructure
                 exclusive: false,
                 autoDelete: false,
                 arguments: null);
+
+            StartConsuming();
         }
 
         public void PublishOrder(OrderMessage orderMessage)
@@ -42,7 +46,7 @@ namespace QueueOrderHub.Infrastructure.Services.Infrastructure
             properties.Persistent = true;
 
             _channel.BasicPublish(
-                exchange: string.Empty,
+                exchange: string.Empty, // defualt Exchange
                 routingKey: _queueName,
                 basicProperties: properties,
                 body: body);
@@ -51,6 +55,37 @@ namespace QueueOrderHub.Infrastructure.Services.Infrastructure
         {
             _channel?.Close();
             _connection?.Close();
+        }
+
+        private void StartConsuming()
+        {
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += (model, ea) =>
+            {
+                try
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    var orderMessage = JsonSerializer.Deserialize<OrderMessage>(message);
+
+                    if (orderMessage is not null)
+                    {
+                        BackgroundJob.Enqueue<OrderProcessingJobs>(x =>
+                            x.ProcessOrderAsync(orderMessage));
+                    }
+
+                    _channel.BasicAck(ea.DeliveryTag, false);
+                }
+                catch (Exception ex)
+                {
+                    _channel.BasicNack(ea.DeliveryTag, false, true);
+                }
+            };
+
+            _channel.BasicConsume(
+                queue: _queueName,
+                autoAck: false,
+                consumer: consumer);
         }
     }
 }
